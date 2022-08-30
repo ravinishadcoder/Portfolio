3036ccc76193b8ede1c59e8a0f56fcf009b7832c
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-08-30 09:41:56 +0530

    update

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Welcome to Ravi's Portfolio!",
     description1: "My name's Ravi Nishad. I'm from Mumbai, Maharastra. I am a Full Stack Developer.",
-    description2: " I am fluent in classics like Javascript. My field of Interest's are building new  Web Technologies and Products and also in areas related to Deep Learning and Natural Launguage Processing. I also apply my passion for developing products with Node.js and Modern Javascript Library, and Frameworks  like React.js and Express.js",
+    description2: "My field of interest's are building new  web applications. I also apply my passion for developing products with Node.js and Modern Javascript Library, and Frameworks  like React.js and Express.js",
     image: 1
 }
