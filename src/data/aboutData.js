405dc5582bcdcf87c1438be7289f8f3d48cb3528
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-10-08 10:36:20 +0530

    Delete src/data directory

--- src/data/aboutData.js
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-    title: "Welcome to Ravi's Portfolio!",
-    description1: "My name's Ravi Nishad. I'm from Mumbai, Maharastra. I am a Full Stack Developer.",
-    description2: "My field of interest's are building new  web applications. I also apply my passion for developing products with Node.js and Modern Javascript Library, and Frameworks  like React.js and Express.js",
-    image: 1
-}
